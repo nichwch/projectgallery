843a44874681dd26580009447e5dc8b0942839bd
nichwch <nichwch@gmail.com>
2024-01-18 14:08:06 -0500

    fix tailwind styles by processing library files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/licks/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
